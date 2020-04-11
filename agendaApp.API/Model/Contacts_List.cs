ad57c4ac644fb3863c31d6fac2764e7c9ddd0f91
OdinPT <leonardo.almeidavieira@gmail.com>
2020-04-11 02:24:29 +0100

    indexing

---
+++ agendaApp.API/Model/Contacts_List.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace agendaApp.API.Model
+{
+    public class Contacts_List
+    {
+        public int MyProperty { get; set; }
+    }
+}
---
+++ agendaApp.API/Model/TypeContact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace agendaApp.API.Model
+{
+    public class TypeContact
+    {
+        [Key]
+        public int IdType { get; set; }
+        public string DescType { get; set; }
+    }
+}
