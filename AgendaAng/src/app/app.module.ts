74795c3d40ad5a7b75f81f647e841dc4ba089478
OdinPT <leonardo.almeidavieira@gmail.com>
2020-04-14 01:44:19 +0100

    login working +-

--- AgendaAng/src/app/app.module.ts
+++ AgendaAng/src/app/app.module.ts
@@ -5,18 +5,27 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './Value/Value.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from '@angular/forms';
+import { AuthService } from './_services/auth.service';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      LoginComponent
    ],
    imports: [
 	 BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+	 AppRoutingModule,
+    HttpClientModule,
+    FormsModule
 	],
-   providers: [],
-   bootstrap: [AppComponent]
+   providers: [
+      AuthService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
